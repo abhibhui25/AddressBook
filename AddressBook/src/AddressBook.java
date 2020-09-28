4e8beb037bee2a750e15e4a091bba86385562c1c
Abhishek Bhui <abhishekbhui11@gmail.com>
2020-09-28 20:55:29 +0530

    Added a class Contacts for contact details

--- AddressBook/src/AddressBook.java
+++ AddressBook/src/AddressBook.java
@@ -1,4 +1,8 @@
 
+class Contacts {
+	String fname, lname, address, city, state, zip, pno, email;
+}
+
 public class AddressBook {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book program ");
