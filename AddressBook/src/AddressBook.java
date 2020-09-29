59cfe680cc36bd6815f4c9d4da930f0fc7e42006
Abhishek Bhui <abhishekbhui11@gmail.com>
2020-09-29 21:58:04 +0530

    Added a method to edit the contact details by name

--- AddressBook/src/AddressBook.java
+++ AddressBook/src/AddressBook.java
@@ -33,13 +33,55 @@ class Contacts {
 		System.out.println("Phone number: " + pno);
 		System.out.println("Email: " + email);
 	}
+
+	public void editContact() {
+		System.out.println("Enter the address");
+		address = sc.next();
+		System.out.println("Enter the city");
+		city = sc.next();
+		System.out.println("Enter the state");
+		state = sc.next();
+		System.out.println("Enter the zip code");
+		zip = sc.next();
+		System.out.println("Enter the phone number");
+		pno = sc.next();
+		System.out.println("Enter the email");
+		email = sc.next();
+	}
 }
 
 public class AddressBook {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book program ");
+		Scanner sc = new Scanner(System.in);
 		Contacts c = new Contacts();
-		c.addContact();
-		c.showContact();
+		int x = 0;
+		while (x < 4) {
+			System.out.println("1.Add a contact");
+			System.out.println("2.Edit a contact by name");
+			System.out.println("3.View contact details by name");
+			System.out.println("4.Exit");
+			int k = sc.nextInt();
+
+			if (k == 1) {
+
+				c.addContact();
+			} else if (k == 2) {
+
+				System.out.println("Enter First Name");
+				String f = sc.next();
+				System.out.println("Enter Last Name");
+				String l = sc.next();
+				String first = c.fname;
+				String last = c.lname;
+				if (first.equals(f) && last.equals(l)) {
+					c.editContact();
+				} else
+					System.out.println("contact not found");
+			} else if (k == 3) {
+				c.showContact();
+			} else if (k == 4)
+				System.exit(0);
+		}
 	}
 }
